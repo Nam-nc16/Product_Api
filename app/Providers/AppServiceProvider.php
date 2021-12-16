c68787a2492b0b37f3418a830ad5441d33d4ee9e
ducnt1003 <nguyentrungduc0310@gmail.com>
2021-12-16 13:50:05 +0700

    message1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Repositories\Admin\Role\RoleRepository;
 use App\Repositories\Admin\Role\RoleRepositoryInterface;
 use App\Repositories\Admin\User\UserRepository;
 use App\Repositories\Admin\User\UserRepositoryInterface;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
